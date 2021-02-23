fb425d3b91142aaa429326ed2f1ee07150c9d956
Markus Rothe <markus.rothe@rite.cc>
2021-02-23 16:24:15 +0000

    Implement BlitzStack
    Currently, the stack is a generic stack to deploy a sample container on
    AWS Fargate, behind a Load Balancer. The missing piece is to build the
    container.

--- lib/blitz-stack.ts
+++ lib/blitz-stack.ts
@@ -1,5 +1,10 @@
 import * as acm from '@aws-cdk/aws-certificatemanager';
+import * as alias from '@aws-cdk/aws-route53-targets';
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
+import * as route53 from '@aws-cdk/aws-route53';
 
 interface BlitzStackProps extends cdk.StackProps {
   certificate: acm.Certificate,
@@ -11,6 +16,79 @@ export class BlitzStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: BlitzStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //
+    // Virtual Private Cloud (i.e. networking)
+    //
+    const vpc = new ec2.Vpc(this, 'vpc', {
+      subnetConfiguration: [
+        { name: 'Public', subnetType: ec2.SubnetType.PUBLIC }
+      ]
+    });
+
+    //
+    // Elastic Container Service (i.e. running the container)
+    //
+    const cluster = new ecs.Cluster(this, 'cluster', {
+      vpc: vpc
+    });
+
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'task-definition', {
+      memoryLimitMiB: 512,
+      cpu: 256
+    });
+
+    const container = taskDefinition.addContainer("container", {
+      // FIXME add real image
+      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample")
+    });
+    container.addPortMappings({ containerPort: 80 });
+
+    // FIXME enable scaling at some later stage
+    const service = new ecs.FargateService(this, 'service', {
+      cluster: cluster,
+      taskDefinition: taskDefinition
+    });
+
+    //
+    // Elastic Load Balancer
+    //
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'load-balancer', {
+      vpc: vpc,
+      internetFacing: true
+    });
+
+    const listenerHttps = lb.addListener('listener-https', {
+      protocol: elbv2.ApplicationProtocol.HTTPS,
+      certificates: [props.certificate]
+    });
+
+    listenerHttps.addTargets('target-group', {
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      port: 80,
+      targets: [service]
+    });
+
+    lb.addListener('listener-http', {
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      port: 80,
+      defaultAction: elbv2.ListenerAction.redirect({
+        protocol: elbv2.ApplicationProtocol.HTTPS,
+        port: '443',
+        permanent: true
+      })
+    });
+
+    const zone = route53.HostedZone.fromLookup(this, 'zone', {
+      domainName: props.domain
+    });
+
+    new route53.ARecord(this, 'load-balancer-a-record', {
+      zone: zone,
+      recordName: props.host,
+      target: route53.RecordTarget.fromAlias(new alias.LoadBalancerTarget(lb))
+    });
+
+    // FIXME AAAA record, currently the VPC has no IPv6 connectivity, needs
+    // support from AWS CDK
   }
 }
